c87e970fd6d377da5ed81e073d532f33c3a13a61
barts <b.steensma@fleetcleaner.com>
2025-03-06 18:33:04 +0100

    fix: "commands" needed to be recreated

--- lua/gitlab.lua
+++ lua/gitlab.lua
@@ -1,12 +1,13 @@
-local gitlab = {}
+local M = {}
 
-function gitlab.plugin_root()
+function M.plugin_root()
   return vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ':p:h:h')
 end
 
-function gitlab.setup(user_config)
+function M.setup(user_config)
   require('gitlab.config').setup(user_config)
   require('gitlab.resource_editing')
+  require('gitlab.commands').create()
 end
 
-return gitlab
+return M
--- plugin/gitlab.lua
+++
@@ -1,8 +0,0 @@
--- Skip loading if this script has already excuted.
-if vim.g.gitlab_plugin_loaded then
-  return
-end
-
-vim.g.gitlab_plugin_loaded = true
-
-require('gitlab.commands').create()

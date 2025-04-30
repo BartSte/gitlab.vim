ab4b45c2188c4b879d0b633dd71b597945103a1c
barts <b.steensma@fleetcleaner.com>
2025-04-30 17:24:51 +0200

    fix: unneeded warning does not show anymore

--- lua/gitlab/ghost_text/lsp.lua
+++ lua/gitlab/ghost_text/lsp.lua
@@ -7,38 +7,38 @@ local M = {}
 --- Setup the client for the module.
 --- @param client table
 function M.setup(client)
-  M.client = client
+    M.client = client
 end
 
 --- Request inline completion.
 --- @param callback fun(err: any, result: any, ctx: any): any
 function M.request_completion(callback)
-  --- @type number
-  local bufnr = vim.api.nvim_get_current_buf()
-  --- @type table
-  local params = vim.tbl_extend('force', { context = {} }, vim.lsp.util.make_position_params())
-  --- @type number
-  local client_id = M.client.client_id
-  --- @type vim.lsp.Client|nil
-  local client = vim.lsp.get_client_by_id(client_id)
-  if not client then
-    return
-  end
-  client.request('textDocument/inlineCompletion', params, callback, bufnr)
+    --- @type number
+    local bufnr = vim.api.nvim_get_current_buf()
+    --- @type number
+    local client_id = M.client.client_id
+    --- @type vim.lsp.Client|nil
+    local client = vim.lsp.get_client_by_id(client_id)
+    if not client then
+        return
+    end
+    --- @type table
+    local params = vim.tbl_extend('force', { context = {} }, vim.lsp.util.make_position_params(0, client.offset_encoding))
+    client.request('textDocument/inlineCompletion', params, callback, bufnr)
 end
 
 --- Notify the client.
 --- @param message string
 --- @param id number
 function M.notify(message, id)
-  --- @type number
-  local client_id = M.client.client_id
-  --- @type vim.lsp.Client|nil
-  local client = vim.lsp.get_client_by_id(client_id)
-  if not client then
-    return
-  end
-  client.notify(message, { id = id })
+    --- @type number
+    local client_id = M.client.client_id
+    --- @type vim.lsp.Client|nil
+    local client = vim.lsp.get_client_by_id(client_id)
+    if not client then
+        return
+    end
+    client.notify(message, { id = id })
 end
 
 return M
